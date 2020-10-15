ff36e0e08e698eb67da47318e46a0193fca619e9
C2y19 <513173606@qq.com>
2020-10-15 08:57:54 +0800

    单位阶跃信号

--- 单位阶跃信号.py
+++ 单位阶跃信号.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
 def step_function1(x=''):
     if x >0:
         return 1
@@ -17,6 +19,7 @@ print(step_function1(m))
 x = np.arange(-5.0,5.0,0.1)  
 y = step_function2(x)
 plt.plot(x,y)
+plt.title("单位阶跃信号")
 plt.ylim(-0.1, 1.1)
 plt.xlabel('t')
 plt.ylabel('u(t)')
