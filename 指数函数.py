c87ae47a9d11b0e09c0d798dca14ece433a1ef55
C2y19 <513173606@qq.com>
2020-10-15 08:51:21 +0800

    指数函数

--- 指数函数.py
+++ 指数函数.py
@@ -6,6 +6,8 @@ e= math.e
 y = e**-x**0.6
 plt.title("指数函数")
 plt.plot(x, y)
+plt.xlabel('t')
+plt.ylabel('x(t)')
 plt.rcParams['font.sans-serif']=['SimHei'] #加上这一句就能在图表中显示中文
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 plt.show()
