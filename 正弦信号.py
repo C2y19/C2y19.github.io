c40ea73a974fdabfad7f894d2cb3287178f01b8f
C2y19 <513173606@qq.com>
2020-10-15 08:47:52 +0800

    。

--- 正弦信号.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
-x=np.arange(0,2*np.pi,0.01)
-y=np.sin(x)
-plt.title("正弦函数")
-plt.plot(x,y)
-plt.show()
